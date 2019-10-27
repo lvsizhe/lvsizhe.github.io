97fc05c41f6a9cc13fbfb90292d6ff8dc383f3bd
keithlv <lvsizhe@gmail.com>
2019-10-27 09:43:44 +0800

    same to above

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 rm _site -rf
-bundle exec jekyll serve --incremental --watch --port 8080 > server.log & 
+bundle exec jekyll serve --incremental --watch --port 8080 > server.log 2>&1 & 
